a12c599a80492c4472aec116c22a154186b6df67
Cihangir SAVAS <cihangir@koding.com>
2017-02-16 16:10:33 +0300

    config: add configuration options for countly

---
+++ config/api.config.js
@@ -0,0 +1,2 @@
+var countlyConfig = require('./config.sample.js');
+module.exports = require('/opt/countly/configextender')(countlyConfig);
---
+++ config/frontend.express.config.js
@@ -0,0 +1,2 @@
+var countlyConfig = require('./countly.config.sample.js');
+module.exports = require('/opt/countly/configextender')(countlyConfig);
